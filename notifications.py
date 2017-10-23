d02bbc40c2d52ff8ca09c599290c1f8b40aeb819
jamessam <jamessam@users.noreply.github.com>
2017-10-22 22:01:11 -0700

    notifications done

--- notifications.py
+++ notifications.py
@@ -10,9 +10,7 @@ from keys import API_KEY, API_SECRET, ACCESS_TOKEN, ACCESS_SECRET
 from keys import AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_REGION
 from keys import DATABASE, SERVER, USERNAME, PASSWORD
 
-
 api = TwitterAPI(API_KEY, API_SECRET, ACCESS_TOKEN, ACCESS_SECRET)
-
 max_id = 920670442926481409
 
 def insert_tweet(c, tweet):
@@ -32,25 +30,35 @@ def insert_tweet(c, tweet):
 
 def get_tweets():
     r = api.request('statuses/mentions_timeline', {'max_id': max_id})
-    data = loads(r.text)
-    if len(data) < 2:
+    tweets = loads(r.text)
+    if len(tweets) < 2:
         yield 'The end'
-    for datum in data:
-        max_id = datum['id']
-        yield datum
+    else:
+        for tweet in tweets:
+            yield tweet
 
-a = open('916408912718565377.json')
-tweet = loads(a.read())
-a.close()
+tweets = get_tweets()
 
 # Connect to database
 connection = mysql.connector.connect(database=DATABASE, host=SERVER, port=3306, user=USERNAME, password=PASSWORD)
 c = connection.cursor()
 
-try:
-    insert_tweet(c, tweet)
-except Exception as e:
-    print(e)
+while(True):
+    try:
+        tweet = next(tweets)
+        if tweet == 'The end':
+            break
+        if tweet['id'] == max_id:
+            continue
+        else:
+            print(tweet['id'])
+            insert_tweet(c, tweet)
+        max_id = tweet['id']
+    except StopIteration:
+        tweets = get_tweets()
+    except TypeError as e:
+        print(e)
+        break
 
 connection.commit()
 c.close()
