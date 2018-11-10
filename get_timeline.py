ee649d8b54ed49eed235a45805dea64f68f69065
James Sam <jamessam@users.noreply.github.com>
2018-11-10 11:00:49 -0800

    refactor get_timeline for DynamoDB.

--- get_timeline.py
+++ get_timeline.py
@@ -1,64 +1,64 @@
-from datetime import datetime
-from json import loads
-from sys import argv
+'''This python3 script copies all available tweets to an AWS DynamoDB datastore.'''
 
+import datetime
+import json
+import os
+
+import boto3
 from TwitterAPI import TwitterAPI
 
-from connection import MySQLConnector
-from keys import API_KEY, API_SECRET, ACCESS_TOKEN, ACCESS_SECRET
-from keys import DATABASE, SERVER, USERNAME
+
+API_KEY=os.environ['API_KEY']
+API_SECRET_KEY=os.environ['API_SECRET_KEY']
+ACCESS_TOKEN=os.environ['ACCESS_TOKEN']
+ACCESS_TOKEN_SECRET=os.environ['ACCESS_TOKEN_SECRET']
+
+DYNAMO_CLIENT = boto3.client('dynamodb')
+DYNAMO_TABLE = os.environ['DYNAMO_TABLE']
+TWITTER_API = TwitterAPI(API_KEY, API_SECRET_KEY, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+
+
+def main():
+    max_id = os.environ['MAX_TWEET_ID']
+    tweets = get_tweets(max_id)
+    while len(tweets) > 1:
+        [insert_tweet(tweet) for tweet in tweets]
+        max_id = tweets[len(tweets)-1]['id']
+        tweets = get_tweets(max_id)
 
 
 def get_tweets(max_id):
-    r = api.request('statuses/user_timeline',
+    r = TWITTER_API.request('statuses/user_timeline',
         { 'screen_name': 'jamessamsf', 'count': 200, 'max_id': max_id })
-    data = loads(r.text)
-    if len(data) < 2:
-        yield 'The end'
-    for datum in data:
-        yield datum
+    return json.loads(r.text)
 
-def insert_tweet(c, tweet):
-    created = datetime.strptime(tweet['created_at'], "%a %b %d %H:%M:%S %z %Y")
-    query = '''
-        INSERT IGNORE INTO statuses (id, created, favorite_count, favorited,
-            in_reply_to_screen_name, in_reply_to_status_id, in_reply_to_user_id,
-            is_quote_status, lang, retweet_count, retweeted, source_app,
-            tweet_text, truncated, tweeter_id)
-        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)'''
-    c.execute(query, (tweet['id'], str(created), tweet['favorite_count'],
-        tweet['favorited'], tweet['in_reply_to_screen_name'],
-        tweet['in_reply_to_status_id'], tweet['in_reply_to_user_id'],
-        tweet['is_quote_status'], tweet['lang'], tweet['retweet_count'],
-        tweet['retweeted'], tweet['source'], tweet['text'],
-        tweet['truncated'], tweet['user']['id'] ))
 
-def main():
-    max_id = 947601267206930432
-    tweets = get_tweets(max_id)
-    good_tweets = []
-
-    with MySQLConnector(USERNAME, argv[1], DATABASE, SERVER) as connection:
-        cursor = connection.cursor()
-
-        while(True):
-            try:
-                tweet = next(tweets)
-                if tweet == 'The end':
-                    break
-                print(tweet['id'])
-                insert_tweet(cursor, tweet)
-                max_id = tweet['id']
-            except StopIteration:
-                connection.commit()
-                tweets = get_tweets(max_id)
-            except TypeError as e:
-                print(e)
-
-        connection.commit()
-        cursor.close()
+def insert_tweet(tweet):
+    print(f"Saving {tweet['id']} to DynamoDB.")
+    DYNAMO_CLIENT.put_item(
+        TableName=DYNAMO_TABLE,
+        Item={
+            'ID': {
+                'N': str(tweet['id'])
+            },
+            'Tweet': {
+                'S': tweet['text']
+            },
+            'ReplyCount': {
+                'N': '0'
+            },
+            'RetweetCount': {
+                'N': str(tweet['retweet_count'])
+            },
+            'FavoriteCount': {
+                'N': str(tweet['favorite_count'])
+            },
+            'TweetAsString': {
+                'S': str(tweet)
+            }
+        }
+    )
 
 
 if __name__ == '__main__':
-    api = TwitterAPI(API_KEY, API_SECRET, ACCESS_TOKEN, ACCESS_SECRET)
     main()
