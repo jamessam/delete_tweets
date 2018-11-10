226bf0743b3088fac43cdfb374083187748dcb2c
James Sam <jamessam@users.noreply.github.com>
2018-11-10 11:02:59 -0800

    remove files that are no longer needed in the nosql architecture

--- connection.py
+++
@@ -1,8 +0,0 @@
-from contextlib import contextmanager
-from mysql.connector import connect
-
-@contextmanager
-def MySQLConnector(user,password,database,host):
-    handle = connect(user=user, password=password, database=database, host=host)
-    yield handle
-    handle.close()
