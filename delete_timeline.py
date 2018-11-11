c15f0751c13369ee47b6bf004a16f4e5b6c0b71c
James Sam <jamessam@users.noreply.github.com>
2018-11-10 17:49:41 -0800

    delete_timeline.py is done, but rather unreliable with the way the scan is set up.

--- delete_timeline.py
+++ delete_timeline.py
@@ -1,30 +1,51 @@
-from sys import argv
+import os
 
-import tweepy
+import boto3
+from boto3.dynamodb.conditions import Key, Attr
+from TwitterAPI import TwitterAPI
 
-from connection import MySQLConnector
-from keys import API_KEY, API_SECRET, ACCESS_TOKEN, ACCESS_SECRET
-from keys import DATABASE, SERVER, USERNAME
 
-query = 'select id, tweet_text from statuses where created <= "2016-06-15" and retweet_count !=0 and favorite_count !=0'
+API_KEY=os.environ['API_KEY']
+API_SECRET_KEY=os.environ['API_SECRET_KEY']
+ACCESS_TOKEN=os.environ['ACCESS_TOKEN']
+ACCESS_TOKEN_SECRET=os.environ['ACCESS_TOKEN_SECRET']
 
-auth = tweepy.OAuthHandler(API_KEY, API_SECRET)
-auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
-api = tweepy.API(auth)
+DYNAMO_CLIENT = boto3.resource('dynamodb')
+DYNAMO_TABLE = DYNAMO_CLIENT.Table(os.environ['DYNAMO_TABLE_ID'])
+TWITTER_API = TwitterAPI(API_KEY, API_SECRET_KEY, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 
-with MySQLConnector(USERNAME, argv[1], DATABASE, SERVER) as connection:
-    cursor = connection.cursor()
-    cursor.execute(query)
-    for tweet in cursor.fetchall():
-        identifier = tweet[0]
-        print(identifier)
-        try:
-            api.destroy_status(identifier)
-        except Exception as e:
-            print(e)
-            # Optional:
-            # cursor.execute('DELETE FROM statuses WHERE id = %s', identifier)
-            # connection.commit()
 
-    connection.commit()
-    cursor.close()
+def main():
+    tweets = get_tweets(int(os.environ['MIN_TWEET_ID']), int(os.environ['MAX_TWEET_ID']))
+    for tweet in tweets:
+        if tweet['ReplyCount'] > 0 or tweet['FavoriteCount'] > 0 or tweet['RetweetCount'] > 0:
+            continue
+        update_deleted_status(tweet)
+        delete_tweet(tweet)
+
+
+def get_tweets(min_id, max_id):
+    fe = Key('ID').gte(min_id) & Attr('ReplyCount').eq('0') & Attr('RetweetCount').eq('0') & Attr('FavoriteCount').eq('0')
+    response = DYNAMO_TABLE.scan(FilterExpression=fe)
+    return response['Items']
+
+
+def update_deleted_status(tweet):
+    DYNAMO_TABLE.update_item(
+        Key = { 'ID': tweet['ID'] },
+        UpdateExpression='SET Deleted = :value',
+        ExpressionAttributeValues={ ':value': True }
+    )
+
+
+def delete_tweet(tweet):
+    tweet_id = tweet['ID']
+    r = TWITTER_API.request(f'statuses/destroy/:{tweet_id}')
+    if r.status_code == 200:
+        print(f'Successfully deleted {tweet_id}')
+    else:
+        print(f'Could not delete {tweet_id}')
+
+
+if __name__ == '__main__':
+    main()
